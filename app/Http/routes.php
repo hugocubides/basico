77faaa4e9e3299dff49eb56f91f16b31e7c36e82
Juan David Cardona <juandavid@ciudadred.co>
2015-05-28 05:16:58 +0000

    agregando el controlador dashboard

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,7 +13,7 @@
 
 Route::get('/', 'WelcomeController@index');
 
-Route::get('home', 'HomeController@index');
+Route::get('dashboard', 'DashboardController@index');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
